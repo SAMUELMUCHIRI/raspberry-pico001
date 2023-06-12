f7f50501bce5060fd8f77bb7d3707165cb75ff09
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-06-12 22:15:58 +0300

    Moving forward

---
+++ learning C for embeded/put.c
@@ -0,0 +1,20 @@
+/*learning some basic functions */
+#include <stdio.h>
+int main()
+{
+    int age;
+    age=34;
+    /*puts("This is just a example ");
+    puts("wow baby steps");*/
+    printf("fortunately i can also display output using printf \n");
+    printf("using forwardslash n creates a escape character \n");
+    printf("But chill! \n Have you \"seen this \"");
+    //printf("\n this is :900");
+    //printf("\n Putting place holder %d\n",24);
+    // printf("\n doing calculations %d plus %d equals %d",2,3,2+3);
+    /* Using placeholders using other signs*/
+    printf("\n Today is a %s , fresh day" ,"Monday");
+    printf("\n The language %c is %d years old",'C',age);
+
+    return(0);
+}
